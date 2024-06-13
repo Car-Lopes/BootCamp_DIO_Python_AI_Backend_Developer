de1b5d13461d149dee0d5db0c6e33fd9a0a7c68d
carlos <car.l1991@yahoo.com.br>
2024-06-13 15:56:13 -0300

    Atualizando

---
+++ Python/Desafios/Data_e_Hora/1_datetime.py
@@ -0,0 +1,23 @@
+from datetime import date, datetime, time
+
+
+data = date(2024, 5, 21)
+print(data)
+print(date.today())
+
+print()
+
+data_hora = datetime(2024, 5, 21, 8, 12, 52)
+data_hora2 = datetime(2024, 5, 21)
+print(data_hora)
+print(data_hora2)
+
+print(datetime.today())
+print(datetime.now())
+
+print()
+
+hora = time(10, 20, 15)
+print(hora)
+print(time.hour)
+
